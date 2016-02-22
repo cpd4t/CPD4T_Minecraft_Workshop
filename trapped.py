09d4779a87517af8a59f7603bae2c3cc997516c2
Neil Ford <neil@neilcford.co.uk>
2016-02-22 14:27:16 +0000

    fixing bugs

--- trapped.py
+++ trapped.py
@@ -4,7 +4,7 @@ import mcpi.block as block
 from time import sleep
 
 # connect python to minecraft
-mc = = minecraft.Minecraft.create()
+mc = minecraft.Minecraft.create()
 
 # clear area in middle of map and move player there
 mc.setBlocks(-60, 0, -60, 60, 50, 60, block.AIR.id)
