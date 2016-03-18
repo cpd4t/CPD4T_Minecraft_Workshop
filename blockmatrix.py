d7094334ce0bc18c3432afcfa9fab4a80d611cdb
Neil Ford <neil@neilcford.uk>
2016-03-18 16:23:59 +0000

    bug fixes

--- blockmatrix.py
+++ blockmatrix.py
@@ -15,8 +15,9 @@ block_id = 0
 z = 0
 
 # clear area in middle of map and move player there
+mc.setBlocks(-60, 0, -60, 60, 50, 60, air)
 mc.setBlocks(-60, -1, -60, 60, -1, 60, stone)
-mc.setBlocks(-60, -2, -60, 60, -2, 60, stone)
+mc.setBlocks(-60, -2, -60, 60, -2, 60, dirt)
 mc.player.setPos(-3, 0, 0)
 
 print("Starting...")
@@ -24,10 +25,10 @@ mc.postToChat("Starting...")
 
 while block_id < 256:
     for x in range(0, 16):
-        print (x, 0, z, block_id)
-        mc.setBlock(x, 0, 7, block_id)
+        print (x, -1, z, block_id)
+        mc.setBlock(x, -1, z, block_id)
         block_id = block_id + 1
-        time.sleep(0.1)
+        sleep(0.1)
     z = z + 1
 
 mc.postToChat("Finished")
